8b477ec50708cef753bcb61b332a6a56957577bb
MartinoySolomon <noy.kakoun@gmail.com>
2025-03-22 11:55:09 +0200

    finished calculator

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const fibNum = document.getElementById("fibNum");
+const resultDiv = document.querySelector(".calculator-result");
+fibNum.addEventListener("keyup", function () {
+	resultDiv.innerHTML = fibonacci(parseInt(fibNum.value));
+});
+
+function fibonacci(n) {
+	if (isNaN(n)) return 0;
+	else
+		return Math.floor(
+			(Math.pow(1 + Math.sqrt(5), n) - Math.pow(1 - Math.sqrt(5), n)) /
+				(Math.pow(2, n) * Math.sqrt(5))
+		);
+}
