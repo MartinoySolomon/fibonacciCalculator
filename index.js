baa71878ee55d7bc4ce8799bb570d0235d5abdc9
MartinoySolomon <noy.kakoun@gmail.com>
2025-03-22 13:32:56 +0200

    version 1.1

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const fibNum = document.getElementById("fibNum");
 const resultDiv = document.querySelector(".calculator-result");
-let x = 8;
-let y = 21;
+const x = 8;
 fibNum.placeholder = x;
-resultDiv.innerHTML = y;
+resultDiv.innerHTML = forFibonacci(x);
 
 
+function forFibonacci(n){
+	if (n==1 || n==2) return 1
+	let a=0, b=1 ,temp;
+	for (let i=2; i<=n;i++){
+		temp=a+b
+		a=b
+		b=temp
+	}
+	return b
+}
+
+function recursiveFibonacci(n){
+	if (n==1 || n==2) return 1
+	else
+	return recursiveFibonacci(n-1)+recursiveFibonacci(n-2)
+}
+
+function fibonacci(n) {
+	if (isNaN(n)) return 0;
+	else
+		return Math.floor(
+			(Math.pow(1 + Math.sqrt(5), n) - Math.pow(1 - Math.sqrt(5), n)) /
+				(Math.pow(2, n) * Math.sqrt(5))
+		);
+}
