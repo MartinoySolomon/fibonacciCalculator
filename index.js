3b22116f41f617ac37e3f2fa8eaf949c3ac2f60f
MartinoySolomon <noy.kakoun@gmail.com>
2025-04-03 12:24:27 +0300

    add loader and reset button

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const fibNum = document.getElementById("fibNum");
 const resultDiv = document.querySelector(".calculator-result");
+const loadingElement=document.querySelector(".loading");
 
-fibNum.addEventListener("blur", function () {
+fibNum.addEventListener("blur", showResult);
+
+function showResult() {
+	resultDiv.classList.remove("hidden");
 	resultDiv.innerHTML = fibonacci(parseInt(fibNum.value));
-});
+};
 
-function fibonacci(n){
-	if (n==1 || n==2) return 1
-	let a=0, b=1 ,temp;
-	for (let i=2; i<=n;i++){
-		temp=a+b
-		a=b
-		b=temp
+function fibonacci(n) {
+	if (n <= 0 || isNaN(n)) hideResult();
+	else if (n == 1 || n == 2) return 1;
+	else {
+		let a = 0,
+			b = 1,
+			temp;
+
+		for (let i = 2; i <= n; i++) {
+			temp = a + b;
+			a = b;
+			b = temp;
+		}
+		return b;
 	}
-	return b
 }
 
-function recursiveFibonacci(n){
-	if (n==1 || n==2) return 1
-	else
-	return recursiveFibonacci(n-1)+recursiveFibonacci(n-2)
+function showLoading(isShow) {
+	loadingElement.classList.toggle("hidden", !isShow);
 }
 
-function binnetFibonacci(n) {
-	if (isNaN(n)) return 0;
-	else
-		return Math.floor(
-			(Math.pow(1 + Math.sqrt(5), n) - Math.pow(1 - Math.sqrt(5), n)) /
-				(Math.pow(2, n) * Math.sqrt(5))
-		);
+
+function hideResult() {
+	resultDiv.classList.add("hidden");
 }
