b1d3cdee39c6ec06e22953447f0850527e8d5a9d
MartinoySolomon <noy.kakoun@gmail.com>
2025-04-03 16:26:33 +0300

    add server using function

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const fibNum = document.getElementById("fibNum");
 const resultDiv = document.querySelector(".calculator-result");
-const loadingElement=document.querySelector(".loading");
+const loadingElement = document.querySelector(".loading");
 
 fibNum.addEventListener("blur", showResult);
 
-function showResult() {
+async function showResult() {
+	const fiboResult = await getFibonacci(parseInt(fibNum.value));
 	resultDiv.classList.remove("hidden");
-	resultDiv.innerHTML = fibonacci(parseInt(fibNum.value));
-};
-
-function fibonacci(n) {
-	if (n <= 0 || isNaN(n)) hideResult();
-	else if (n == 1 || n == 2) return 1;
-	else {
-		let a = 0,
-			b = 1,
-			temp;
-
-		for (let i = 2; i <= n; i++) {
-			temp = a + b;
-			a = b;
-			b = temp;
-		}
-		return b;
-	}
+	resultDiv.textContent = `${fiboResult.result}`;
 }
 
 function showLoading(isShow) {
 	loadingElement.classList.toggle("hidden", !isShow);
 }
 
-
 function hideResult() {
 	resultDiv.classList.add("hidden");
+}
+
+async function getFibonacci(number) {
+	try {
+		hideResult();
+		showLoading(true);
+		const fetchNumber = await fetch(
+			`http://localhost:3000/calculate/${number}`
+		);
+		if (!fetchNumber.ok) {
+			throw new Error("not a number");
+		}
+		const jsonNumber = await fetchNumber.json();
+		console.log(jsonNumber);
+		return jsonNumber;
+	} catch (err) {
+		console.log("error", err);
+	} finally {
+		showLoading(false);
+	}
 }
