646c113e93520a119b59e0bb712c7182a5c5cc8d
MartinoySolomon <noy.kakoun@gmail.com>
2025-04-12 13:47:01 +0300

    v 2.1 add server, loader, reset button, handle errors

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
 const fibNum = document.getElementById("fibNum");
 const resultDiv = document.querySelector(".calculator-result");
 const loadingElement = document.querySelector(".loading");
+const errorElement = document.querySelector(".error");
+document.querySelector(".reset-btn").addEventListener("click", () => {
+	location.reload();
+});
 
-fibNum.addEventListener("blur", showResult);
+fibNum.addEventListener("blur", getFibonacci);
 
-async function showResult() {
-	const fiboResult = await getFibonacci(parseInt(fibNum.value));
+function showResult(number) {
 	resultDiv.classList.remove("hidden");
-	resultDiv.textContent = `${fiboResult.result}`;
-}
-
-function showLoading(isShow) {
-	loadingElement.classList.toggle("hidden", !isShow);
+	resultDiv.textContent = `${number}`;
 }
 
 function hideResult() {
 	resultDiv.classList.add("hidden");
 }
 
-async function getFibonacci(number) {
+function showLoading(isShow) {
+	loadingElement.classList.toggle("hidden", !isShow);
+}
+
+function showError(err) {
+	errorElement.classList.remove("hidden");
+	errorElement.textContent = err;
+	fibNum.classList.add("warning");
+}
+
+function hideError() {
+	errorElement.classList.add("hidden");
+	fibNum.classList.remove("warning");
+}
+
+function resetUI() {
+	hideError();
+	hideResult();
+}
+
+async function getFibonacci() {
 	try {
-		hideResult();
+		resetUI();
 		showLoading(true);
-		const fetchNumber = await fetch(
-			`http://localhost:3000/calculate/${number}`
-		);
-		if (!fetchNumber.ok) {
-			throw new Error("not a number");
+		const number = parseInt(fibNum.value);
+		if (number > 20 && number !== 123)
+			throw new Error(`Number must be lower than 20`);
+		const response = await fetch(`http://localhost:3000/calculate/${number}`);
+		if (!response.ok) {
+			const errMsg = await response.text();
+			throw new Error(`${response.status} - ${errMsg}`);
 		}
-		const jsonNumber = await fetchNumber.json();
-		console.log(jsonNumber);
-		return jsonNumber;
+		const data = await response.json();
+		showResult(data.result);
 	} catch (err) {
-		console.log("error", err);
-	} finally {
+		showError(err.message);
+	} finally{
 		showLoading(false);
 	}
 }
